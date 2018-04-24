118a8cbf0a2dbda0fa7e988517e964d5606d091c
firmiana-caesar <firmiana-caesar@outlook.com>
2018-04-24 22:00:10 +0800

    take picture

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -3,7 +3,8 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    picturePath: null
   },
 
   bindViewTap: function(){
@@ -11,8 +12,23 @@ Page({
       url: '../index/index'
     })*/
     wx.navigateBack({
-      
+
     })
+  },
+
+  TapTakePicture: function(){var _this = this
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function(res) {
+        //picturePath = res.tempFilePaths
+        _this.setData({
+          picturePath:res.tempFilePaths
+        })
+      },
+    })
+
   },
 
   onLoad: function () {
